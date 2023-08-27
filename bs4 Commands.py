f9a1e7a1c1ee0043ead7f4311b4dedd350702a48
Prableen Singh Matharu <130837233+Prabhsingh0401@users.noreply.github.com>
2023-08-27 21:49:45 +0530

    Update bs4 Commands.py

--- bs4 Commands.py
+++ bs4 Commands.py
@@ -8,7 +8,22 @@ soup = BeautifulSoup(html_doc, 'html.parser') # here beautiful soap is going to 
 
 #COMMANDS FOR BS4 
 
-# print(soup.prettify())  # the file is read by this command 
-# print(soup.title.string, type(soup.title.string))  # Gives the title of html file - type gives the type of title 
-# print (soup.div)  # this only gives the first Div also you can replace the word div with any element like a , p  etc 
+print(soup.prettify())  # the file is read by this command 
+
+print(soup.title.string, type(soup.title.string))  # Gives the title of html file - type gives the type of title 
+
+print (soup.div)  # this only gives the first Div also you can replace the word div with any element like a , p  etc 
+
 print (soup.find_all("div"))  # to get all the divs and element entered in bracket 
+
+for link in soup.find_all('a'):
+    print(link.get("href"))         # To get all the links in html
+    
+for link in soup.find_all('a'):
+    print(link.get_text())          # To get the text enclosed in anchor tag 
+
+print(soup.find_all(class_="icon"))     
+                                    # To find the data related to particular class and adding all to find we get all the classes 
+
+for child in soup.find(class_="container").children:
+    print(child)                    # To get child boxes in div classes       
